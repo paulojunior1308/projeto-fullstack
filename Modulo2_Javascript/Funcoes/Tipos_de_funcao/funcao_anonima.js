8aee1f42c2c53397df03154deb713aabde7d1cb8
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-12 09:39:41 -0300

    Funcoes em Javascript

---
+++ Modulo2_Javascript/Funcoes/Tipos_de_funcao/funcao_anonima.js
@@ -0,0 +1,8 @@
+const soma = function(a,b)
+{
+  return a + b;
+}
+
+soma(1,2) //3
+soma(5,4) //9
+
---
+++ Modulo2_Javascript/Funcoes/Tipos_de_funcao/funcao_autoinvocavel.js
@@ -0,0 +1,26 @@
+(
+  function()
+  {
+    let name = "Digital Innovation One";
+    return name;
+  }
+)();
+
+
+(
+  function(a, b)
+  {
+    return a + b;
+  }
+)(1,2);
+
+
+const soma3 = 
+(
+  function()
+  {
+    return a + b;
+  }
+)(1, 2);
+
+console.log(soma3); //3
