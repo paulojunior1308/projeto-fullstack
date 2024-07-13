4620097c83420b31dd59df8606cd7c68f7d1f052
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-13 11:03:32 -0300

    Aula pratica em Javascript

---
+++ Modulo2_Javascript/Orientacao_Objetos/Atividade-pratica/conta_bancaria.js
@@ -0,0 +1,86 @@
+class contaBancaria
+{
+  constructor(agencia, numero, tipo)
+  {
+    this.agencia = agencia;
+    this.numero = numero;
+    this.tipo = tipo;
+    this._saldo = 0;
+  }
+
+  get saldo()
+  {
+    return this._saldo;
+  }
+
+  set saldo(valor)
+  {
+    this._saldo = valor;
+  }
+
+  sacar(valor)
+  {
+    if (valor > this._saldo)
+      {
+        return "Operação negada";
+      }
+    this._saldo = this._saldo - valor;
+
+    return this._saldo;
+  }
+
+  deposita(valor)
+  {
+    this._saldo = this._saldo + valor;
+
+    return this._saldo;
+  }
+}
+
+class contaCorrente extends contaBancaria
+{
+  constructor(agencia, numero, cartaoCredito)
+  {
+    super(agencia, numero);
+    this.tipo = "corrente";
+    this._cartaoCredito = cartaoCredito;
+  }
+
+  get cartaoCredito()
+  {
+    return this._cartaoCredito;
+  }
+
+  set cartaoCredito(valor)
+  {
+    return this._cartaoCredito = valor;
+  }
+}
+
+class contaPoupanca extends contaBancaria
+{
+  constructor(agencia, numero )
+  {
+    super(agencia, numero);
+    this.tipo = "poupança";
+  }
+}
+
+class contaUniversitaria extends contaBancaria
+{
+  constructor(agencia, numero)
+  {
+    super(agencia, numero);
+    this.tipo = "universitaria";
+  }
+
+  sacar(valor)
+  {
+    if (valor < 500)
+    {
+      return "Operação negada!"
+    }
+
+    this._saldo = this._saldo - valor;
+  }
+}
