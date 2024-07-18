c7a51e047a7bbae326112e749fdbe9b4788467b1
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-18 13:21:28 -0300

    ReactJS - Hooks

---
+++ Modulo3_ReactJS/React-Hooks/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // ou './src/index.jsx' dependendo do seu arquivo de entrada
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(mp3|wav)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 8080,
+  }
+};
