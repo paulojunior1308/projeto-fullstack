09d77b870feac685af1f3bc1aff492a49b133986
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-19 16:29:11 -0300

    Introducao ao Redux e fluxos da arquitetura ReactJS

--- Modulo3_ReactJS/Internet-ReactJS/redux-flux/src/App.js
+++ Modulo3_ReactJS/Internet-ReactJS/redux-flux/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import Counter from './Counter';
 
-function App() {
+const ClassRoom = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const ClassRoomControls = styled.div`
+	display: flex;
+	justify-content: space-around;
+	margin: 10px 0;
+`;
+
+const ClassRoomBoard = styled.div`
+	margin: 12px;
+	display: flex;
+	justify-content: center;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ClassRoom>
+      <ClassRoomControls>
+        Tópico único ;)
+      </ClassRoomControls>
+      <ClassRoomBoard>
+        <Counter />
+      </ClassRoomBoard>
+    </ClassRoom>
   );
 }
 
