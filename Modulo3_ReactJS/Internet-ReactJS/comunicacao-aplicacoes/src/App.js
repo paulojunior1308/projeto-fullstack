e03570c30fc5b08fac048c73f3ae53c7e290228f
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-19 17:23:27 -0300

    Comunicacao avançada entre aplicacoes ReactJS

--- Modulo3_ReactJS/Internet-ReactJS/comunicacao-aplicacoes/src/App.js
+++ Modulo3_ReactJS/Internet-ReactJS/comunicacao-aplicacoes/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Topico1 } from './Components/Topico1';
+import Topico3 from './Components/Topico3/Topico3';
 
-function App() {
+const ClassRoom = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const ClassRoomControls = styled.div`
+	display: flex;
+	justify-content: space-around;
+	margin: 10px 0;
+`;
+
+const ClassRoomBoard = styled.div`
+	margin: 12px;
+	display: flex;
+	justify-content: center;
+`;
+
+const App = () => {
+  const [topic, setTopic] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ClassRoom>
+      <ClassRoomControls>
+        <button onClick={() => setTopic(1)}>Tópico 1</button>
+        <button onClick={() => setTopic(3)}>Tópico 3</button>
+      </ClassRoomControls>
+      <ClassRoomBoard>
+        {topic === 1 && <Topico1 />}
+        {topic === 3 && <Topico3 />}
+      </ClassRoomBoard>
+    </ClassRoom>
   );
 }
 
