136c1295965ca9ea3f6a6f3f172a5f4504fa3e1e
paulojunior1308 <pauloesjr2@gmail.com>
2024-08-03 16:55:07 -0300

    Singleton ou Prototype

---
+++ Modulo5_Spring/singleton-prototype/src/test/java/dio/singletonprototype/SingletonPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.singletonprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingletonPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
