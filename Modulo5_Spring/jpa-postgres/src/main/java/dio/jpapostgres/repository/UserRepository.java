818efddb69a4e2a672e930bbad0e37a30a9608ca
paulojunior1308 <pauloesjr2@gmail.com>
2024-08-04 14:01:38 -0300

    commit

--- Modulo5_Spring/jpa-postgres/src/main/java/dio/jpapostgres/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dio.jpapostgres.repository;
-
-import dio.jpapostgres.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
